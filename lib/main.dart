124c55279a9c2a796a5f185fe7bbf35b1214d42e
Muhammed Emir Cihangir <69801024+emircihangir@users.noreply.github.com>
2025-02-23 18:10:58 +0300

    Finished the first slide in welcomeView

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,73 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const AndroidApp());
+  runApp(const PrivateNotesApp());
 }
 
-class AndroidApp extends StatelessWidget {
-  const AndroidApp({super.key});
+class PrivateNotesApp extends StatelessWidget {
+  const PrivateNotesApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return CupertinoApp(
+      theme: CupertinoThemeData(textTheme: CupertinoTextThemeData(textStyle: TextStyle(fontFamily: "SFpro", color: CupertinoColors.black))),
       home: CupertinoPageScaffold(
         child: SafeArea(
-          child: loginPage(),
+          child: Padding(
+            padding: const EdgeInsets.all(8.00),
+            child: welcomeView(),
+          ),
         ),
       ),
     );
   }
 }
 
-Widget loginPage() {
-  return Placeholder();
+Widget welcomeView() {
+  var pageController = PageController(initialPage: 0);
+  return PageView(
+    controller: pageController,
+    children: [
+      Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image(
+            image: AssetImage("assets/private-notes-icon.png"),
+            width: 200,
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          Text(
+            "Welcome to Private Notes.",
+            style: TextStyle(fontSize: 24),
+          ),
+          CupertinoButton(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text("Let's get started "),
+                Icon(CupertinoIcons.arrow_right)
+              ],
+            ),
+            onPressed: () {
+              pageController.nextPage(duration: Durations.long1, curve: Curves.ease);
+            },
+          )
+        ],
+      ),
+      Placeholder(
+        child: Center(
+          child: Text("Slide 2"),
+        ),
+      ),
+      Placeholder(
+        child: Center(
+          child: Text("Slide 3"),
+        ),
+      ),
+    ],
+  );
 }
