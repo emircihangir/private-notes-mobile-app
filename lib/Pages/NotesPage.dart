85ac38d51da081b118b508aa979d162e176a3ffb
Muhammed Emir Cihangir <69801024+emircihangir@users.noreply.github.com>
2025-03-01 13:07:41 +0300

    Seperated the notesPage

---
+++ lib/Pages/NotesPage.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/cupertino.dart';
+import 'package:privatenotes/main.dart';
+
+Widget notesPage(BuildContext context) {
+  return CupertinoPageScaffold(
+    navigationBar: CupertinoNavigationBar(
+      leading: CupertinoButton(
+          sizeStyle: CupertinoButtonSize.small,
+          child: Icon(
+            CupertinoIcons.lock,
+            size: 24,
+          ),
+          onPressed: () {
+            //TODO: Encrypt the notes before leaving.
+            Navigator.of(context).pushAndRemoveUntil(
+              PageRouteBuilder(
+                pageBuilder: (context, animation, secondaryAnimation) {
+                  return loginPage(context);
+                },
+                transitionsBuilder: (context, animation, secondaryAnimation, child) {
+                  var fadeAnimation = Tween(begin: 0.0, end: 1.0).animate(animation);
+                  return FadeTransition(opacity: fadeAnimation, child: child);
+                },
+              ),
+              (Route<dynamic> route) => false,
+            );
+          }),
+      middle: Text("Notes"),
+      trailing: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          CupertinoButton(
+              sizeStyle: CupertinoButtonSize.small,
+              child: Icon(
+                CupertinoIcons.text_badge_plus,
+                size: 24,
+              ),
+              onPressed: () => createNewNote(context)),
+          CupertinoButton(
+              sizeStyle: CupertinoButtonSize.small,
+              child: Icon(
+                CupertinoIcons.ellipsis_circle,
+                size: 24,
+              ),
+              onPressed: () {
+                showCupertinoModalPopup(
+                  context: context,
+                  builder: (context) => settingsModal(),
+                );
+              }),
+        ],
+      ),
+    ),
+    child: SafeArea(child: noNotesView(context)),
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:privatenotes/Pages/NotesPage.dart';
 import './disclaimer.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -202,60 +203,6 @@ Widget welcomeView(BuildContext context) {
         ),
       ),
     ),
-  );
-}
-
-Widget notesPage(BuildContext context) {
-  return CupertinoPageScaffold(
-    navigationBar: CupertinoNavigationBar(
-      leading: CupertinoButton(
-          sizeStyle: CupertinoButtonSize.small,
-          child: Icon(
-            CupertinoIcons.lock,
-            size: 24,
-          ),
-          onPressed: () {
-            //TODO: Encrypt the notes before leaving.
-            Navigator.of(context).pushAndRemoveUntil(
-              PageRouteBuilder(
-                pageBuilder: (context, animation, secondaryAnimation) {
-                  return loginPage(context);
-                },
-                transitionsBuilder: (context, animation, secondaryAnimation, child) {
-                  var fadeAnimation = Tween(begin: 0.0, end: 1.0).animate(animation);
-                  return FadeTransition(opacity: fadeAnimation, child: child);
-                },
-              ),
-              (Route<dynamic> route) => false,
-            );
-          }),
-      middle: Text("Notes"),
-      trailing: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          CupertinoButton(
-              sizeStyle: CupertinoButtonSize.small,
-              child: Icon(
-                CupertinoIcons.text_badge_plus,
-                size: 24,
-              ),
-              onPressed: () => createNewNote(context)),
-          CupertinoButton(
-              sizeStyle: CupertinoButtonSize.small,
-              child: Icon(
-                CupertinoIcons.ellipsis_circle,
-                size: 24,
-              ),
-              onPressed: () {
-                showCupertinoModalPopup(
-                  context: context,
-                  builder: (context) => settingsModal(),
-                );
-              }),
-        ],
-      ),
-    ),
-    child: SafeArea(child: noNotesView(context)),
   );
 }
 
