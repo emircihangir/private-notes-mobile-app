556d8edec221f0dbf50df26f83ec916ad5bac352
Muhammed Emir Cihangir <69801024+emircihangir@users.noreply.github.com>
2025-03-02 15:00:09 +0300

    Added the ability to initialize the data files

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'dart:convert';
+import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:privatenotes/Pages/login_page.dart';
-import 'package:privatenotes/Pages/note_page.dart';
 import 'package:privatenotes/Pages/notes_page.dart';
 import 'package:privatenotes/Views/welcome_view.dart';
 import 'package:provider/provider.dart';
@@ -10,6 +12,7 @@ import 'package:provider/provider.dart';
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
 Uint8List? digestBytes;
+File? cookiesFile;
 
 class DisclaimerCBModel extends ChangeNotifier {
   bool? _value = false;
@@ -20,7 +23,7 @@ class DisclaimerCBModel extends ChangeNotifier {
   }
 }
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
     statusBarColor: Colors.transparent,
@@ -29,6 +32,33 @@ void main() {
     systemNavigationBarColor: CupertinoColors.systemBackground,
     systemNavigationBarIconBrightness: Brightness.dark,
   ));
+
+  // create the necessary files if they do not exists.
+  final directory = await getApplicationDocumentsDirectory();
+  final notesFilePath = "${directory.path}/notes.json";
+  final cookiesFilePath = "${directory.path}/cookies.json";
+  File notesFile = File(notesFilePath);
+  cookiesFile = File(cookiesFilePath);
+
+  if (await notesFile.exists() == false) {
+    // create the notes file.
+    Map<String, dynamic> notesFileData = {
+      "notes": {}
+    };
+    final notesFileDataJSON = json.encode(notesFileData);
+    await notesFile.writeAsString(notesFileDataJSON);
+    print("notes.json successfully created.");
+  }
+  if (await cookiesFile!.exists() == false) {
+    // create the cookies file.
+    Map<String, dynamic> cookiesFileData = {
+      "totalNotes": 0
+    };
+    final cookiesFileDataJSON = json.encode(cookiesFileData);
+    await cookiesFile!.writeAsString(cookiesFileDataJSON);
+    print("cookies.json successfully created.");
+  }
+
   runApp(const PrivateNotesApp());
 }
 
