cf9a77780ed8417b6481e6c866159a5d082ede5e
Muhammed Emir Cihangir <69801024+emircihangir@users.noreply.github.com>
2025-03-07 15:39:56 +0300

    Improved the dummy notes view

--- lib/Views/dummy_notes_view.dart
+++ lib/Views/dummy_notes_view.dart
@@ -2,6 +2,14 @@
 import 'package:flutter/cupertino.dart';
 
 Widget dummyNotesView(BuildContext context) {
+  List<Widget> dummyNotes() {
+    List<Widget> widgets = [];
+    for (var i = 0; i < 10; i++) {
+      widgets.add(CupertinoListTile(title: Text("Note ${i + 1}")));
+    }
+    return widgets;
+  }
+
   return SingleChildScrollView(
     child: Column(
       children: [
@@ -10,81 +18,7 @@ Widget dummyNotesView(BuildContext context) {
           topMargin: 0,
           hasLeading: false,
           backgroundColor: CupertinoColors.systemBackground,
-          children: [
-            CupertinoListTile(
-              title: Text("note 1"),
-              subtitle: Text("note 1 subtitle"),
-              onTap: () {},
-            ),
-            CupertinoListTile(
-              title: Text("note 2"),
-              subtitle: Text("note 2 subtitle"),
-            ),
-            CupertinoListTile(
-              title: Text("note 2"),
-              subtitle: Text("note 2 subtitle"),
-            ),
-            CupertinoListTile(
-              title: Text("note 2"),
-              subtitle: Text("note 2 subtitle"),
-            ),
-            CupertinoListTile(
-              title: Text("note 2"),
-              subtitle: Text("note 2 subtitle"),
-            ),
-            CupertinoListTile(
-              title: Text("note 2"),
-              subtitle: Text("note 2 subtitle"),
-            ),
-            CupertinoListTile(
-              title: Text("note 2"),
-              subtitle: Text("note 2 subtitle"),
-            ),
-            CupertinoListTile(
-              title: Text("note 2"),
-              subtitle: Text("note 2 subtitle"),
-            ),
-            CupertinoListTile(
-              title: Text("note 2"),
-              subtitle: Text("note 2 subtitle"),
-            ),
-            CupertinoListTile(
-              title: Text("note 2"),
-              subtitle: Text("note 2 subtitle"),
-            ),
-            CupertinoListTile(
-              title: Text("note 2"),
-              subtitle: Text("note 2 subtitle"),
-            ),
-            CupertinoListTile(
-              title: Text("note 2"),
-              subtitle: Text("note 2 subtitle"),
-            ),
-            CupertinoListTile(
-              title: Text("note 2"),
-              subtitle: Text("note 2 subtitle"),
-            ),
-            CupertinoListTile(
-              title: Text("note 2"),
-              subtitle: Text("note 2 subtitle"),
-            ),
-            CupertinoListTile(
-              title: Text("note 2"),
-              subtitle: Text("note 2 subtitle"),
-            ),
-            CupertinoListTile(
-              title: Text("note 2"),
-              subtitle: Text("note 2 subtitle"),
-            ),
-            CupertinoListTile(
-              title: Text("note 2"),
-              subtitle: Text("note 2 subtitle"),
-            ),
-            CupertinoListTile(
-              title: Text("note 2"),
-              subtitle: Text("note 2 subtitle"),
-            ),
-          ],
+          children: dummyNotes(),
         ),
       ],
     ),
